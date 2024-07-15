9403b56f0900f1f4e2679048c489860a7c8e9a81
Martin <github@msturm.net>
2024-07-15 22:24:42 +0200

    fix empty start page for non-staff users

--- index.php
+++ index.php
@@ -10,9 +10,10 @@ include "login/misc/pagehead.php";
 if ($auth->isLoggedIn()) {
     if ($auth->hasRole("Accountant")) {
         echo "<meta http-equiv='refresh' content='0; url=/page_accountant_overview.php' />";
-    }
-    if ($auth->hasRole("Staff")) {
+    } else if ($auth->hasRole("Staff")) {
         echo "<meta http-equiv='refresh' content='0; url=/page_user_track.php' />";
+    } else {
+        echo "<meta http-equiv='refresh' content='0; url=/user/accountedit.php' />";
     }
 } else {
     echo "<meta http-equiv='refresh' content='0; url=login/index.php' />";
